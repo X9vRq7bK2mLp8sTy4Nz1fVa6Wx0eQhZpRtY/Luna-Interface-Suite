58bfe8a5e3c31285e8eb12985ed08b812f5fe36e
LeoTLua <skibidirizztoilet80@gmail.com>
2025-06-12 17:13:36 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -2568,7 +2568,7 @@ function Luna:CreateWindow(WindowSettings)
 		-- Stolen From Sirius Stuff Begins Here
 
 		HomeTabPage.detailsholder.dashboard.Discord.Interact.MouseButton1Click:Connect(function()
-			setclipboard(tostring("No Discord Yet"..HomeTabSettings.DiscordInvite)) -- Hunter if you see this I added copy also was too lazy to send u msg
+			setclipboard(tostring("https://dsc.gg/astrolua"..HomeTabSettings.DiscordInvite)) -- Hunter if you see this I added copy also was too lazy to send u msg
 			if request then
 				request({
 					Url = 'http://127.0.0.1:6463/rpc?v=1',
